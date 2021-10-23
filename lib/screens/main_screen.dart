cbca36a37217676583a60ff023c1f5bff8aba7f8
Kemal <kemalhasan03.03@gmail.com>
2021-10-23 11:03:55 +0300

    update

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -169,7 +169,7 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
 
-    
+
     return SafeArea(
       child: Scaffold(
         resizeToAvoidBottomInset: false,
